c6e379e31d2594a043fe841e812fe2b9a34214ca
Hugo Firth <me@hugofirth.com>
2016-07-15 10:26:53 -0400

    Did some more work on the experiment trait. Now on to the implementations

--- src/main/scala/org/gdget/loom/experimental/Experiment.scala
+++ src/main/scala/org/gdget/loom/experimental/Experiment.scala
@@ -16,32 +16,51 @@
   * limitations under the License.
   */
 
+import scala.concurrent.Future
+ 
+import org.gdget.data.query._
+import org.gdget.Edge
+import org.gdget.partitioned._
+
  /** Experiment trait to hold implementation common to all experiments (e.g. IO).
    *
    */
 sealed trait Experiment[S[_], V, E[_]] {
+
+  /** Type Aliases for conciceness and clarity */
+  type Q = QueryIO[LogicalParGraph[S, ?, ?[_]], V, E, _]
+  type QStream = Stream[Q]
 
   /** Typeclass instances for S & E */
   implicit def S: ParScheme[S]
   implicit def E: Edge[E]
 
   /** Map of query idenifiers to queries themselves (QueryIO objects) */
-  def queries: Map[String, QueryIO[LogicalParGraph[S, ?, ?[_]], V, E, _]]
+  def queries: Map[String, Q]
 
   /** Stream of queries made up from the values from `queries`.
     * 
     * The frequency of each distinct query pattern may change over time.
     */
-  def queryStream: Stream[QueryIO[LogicalParGraph[S, ?, ?[_]], V, E, _]] 
+  def queryStream: QStream
 
+  /** Takes any function from a graph to a return type A, and lazily measures the execution
+    * time, returning a function from a graph  to a tuple (Int, A). 
+    *
+    * TODO: use cats.Eval here
+    */
+  def time[A](f: (LogicalParGraph[S, V, E], QStream) => A): (LogicalParGraph[S, V, E], QStream) => (Int, A)
   
-  //time function: ((G, Q) => A): (Int, A) ... or Make an "Instrumented" Applicative?
-  
-  //run function which produces a result.... where are we doing IO?
-  
+  /** Key method of the Experiment trait which takes a Graph object and retuns a Future of the Experiment result.
+    *  
+    * As queries in the workload are read only, individual queries may be evaluated in their own threads. The Future
+    * result of each is then combined and returned as Future[A: Monoid] has a Monoid.
+    */ 
+  def run(g: LogicalParGraph[S, V, E]): Future[Experiment.Result] 
 }
 
 object Experiment {
   //Result case class, which takes Time and IPT at a minimum
+  case class Result(time: Int, ipt: Int)
 }
 
